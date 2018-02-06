14a9324abb5ef0047c301ef90a15aa56f37eeb94
Chris Whiting <chris.whiting@panasonic.aero>
2018-02-06 07:37:23 -0800

    feat: Add comment reducer, add all actions to appropriate reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -44,6 +44,14 @@ function post(state = initialPosts, action) {
   switch(action.type) {
     case GET_ALL_POSTS:
       return { ...state };
+    case ADD_POST:
+      return {};
+    case DELETE_POST:
+      return {};
+    case EDIT_POST:
+      return {};
+    case GET_ONE_POST:
+      return {};
     default:
       return state;
   }
@@ -51,6 +59,25 @@ function post(state = initialPosts, action) {
 
 function category(state = initialCategories, action) {
   switch(action.type) {
+    case GET_ALL_CATEGORIES:
+      return {};
+    case GET_ONE_CATEGORY:
+      return {};
+    default:
+      return state;
+  }
+}
+
+function comment(state = {}, action) {
+  switch(action.type) {
+    case GET_ALL_COMMENTS:
+      return {};
+    case EDIT_COMMENT:
+      return {};
+    case ADD_COMMENT:
+      return {};
+    case DELETE_COMMENT:
+      return {};
     default:
       return state;
   }
@@ -58,5 +85,6 @@ function category(state = initialCategories, action) {
 
 export default combineReducers({
   post,
-  category
+  category,
+  comment
 });
