4cd6a5771d548874924ebaa7401d1c1544960fea
Chris Whiting <chris.whiting@panasonic.aero>
2018-03-02 07:22:39 -0800

    feat: Add delete buttons to posts

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -5,6 +5,7 @@ import { deletePost } from '../actions';
 
 class Post extends Component {
   render() {
+    let postDate = new Date(this.props ? this.props.timestamp : 0);
     return (
       // <div>
       //   {this.props.name}
@@ -12,14 +13,21 @@ class Post extends Component {
       //     {this.props.title}
       //   </Link>
       // </div>
+      <div>
       <li className="collection-item left flow-text">
         <div className="left-align">
-          <button type="submit" onClick={() => this.props.delete_post(this.props.id)} className="btn btn-primary">Delete</button>
+          <button type="submit" onClick={() => this.props.delete_post(this.props.id)} className="btn-floating btn-tiny waves-effect waves-light red">
+            <i className="material-icons tiny">close</i>
+          </button>
+          <div className="width-divider"/>
           <Link to={"/post/" + this.props.id} className="secondary-content">
             {this.props.title}
           </Link>
         </div>
       </li>
+      <p>Submitted by {this.props ? this.props.author : "Undefined Author"} at {postDate.toUTCString()}</p>
+    </div>
+
     );
   };
 }
