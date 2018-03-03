54374f4f7f6a30bac37784971213502135025b6b
Chris Whiting <chris.whiting@panasonic.aero>
2018-03-02 16:47:11 -0800

    feat: Add category filter button, remove categories component from main container

--- frontend/src/containers/MainContainer.js
+++ frontend/src/containers/MainContainer.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
-import { getAllPosts, getAllCategories } from '../actions';
+import { getAllPosts, getAllCategories, getPostsForCategory } from '../actions';
 import { Link } from 'react-router-dom';
 
-import Categories from '../components/Categories';
+// import Categories from '../components/Categories';
 import Posts from '../components/Posts';
+import shortid from 'shortid';
+
 
 class MainContainer extends Component {
   componentDidMount() {
@@ -18,15 +20,26 @@ class MainContainer extends Component {
       <div>
         {/* <h1>Main Container!</h1> */}
         <div className="row">
-          <div className="col s4">
-            <Categories
-              categories={this.props.categories}
-            />
-          </div>
-          <div className="col s8">
+          <div className="col s12">
+            <Link className='dropdown-button btn' href='#' to="/" data-activates='dropdown1'>Filter By Category</Link>
+
             <Posts
               posts={this.props.posts}
             />
+
+            <ul id='dropdown1' className='dropdown-content'>
+              {Object.keys(this.props.categories).map(catKey => {
+                let category = this.props.categories[catKey];
+                return (
+                <li key={shortid.generate()}>
+                  <Link className="btn blue" to="/">
+                  <button onClick={() => this.props.get_posts_for_category(this.props.name)}>
+                    {category.name}
+                  </button>
+                  </Link>
+                </li>
+              )})}
+            </ul>
           </div>
         </div>
         {/* <button onClick={() => this.props.get_posts()}> Posts</button>
@@ -51,7 +64,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     get_posts: () => dispatch(getAllPosts()),
-    get_categories: () => dispatch(getAllCategories())
+    get_categories: () => dispatch(getAllCategories()),
+    get_posts_for_category: (category) => dispatch(getPostsForCategory(category)),
   }
 }
 
