3277d52a9cb5f17bf81025283fc2d5fe076cbea7
Chris Whiting <chris.whiting@panasonic.aero>
2018-03-06 17:32:38 -0800

    feat: hook in delete comment action and reducer, add navigation after submission

--- frontend/src/components/PostDetail.js
+++ frontend/src/components/PostDetail.js
@@ -6,11 +6,12 @@ import PostComments from '../containers/Posts/PostComments';
 
 class PostsDetail extends Component {
   render() {
+    console.log("Post Detail props: ", this.props)
     return (
     <div>
       <PostTitle root_id={this.props.match.params.id}/>
       <PostVoteAndBody root_id={this.props.match.params.id}/>
-      <PostComments root_id={this.props.match.params.id}/>
+      <PostComments history={this.props.history} root_id={this.props.match.params.id}/>
     </div>
     )
   };
--- frontend/src/containers/Posts/PostComments.js
+++ frontend/src/containers/Posts/PostComments.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { getAllPosts, getAllComments } from '../../actions';
+import { getAllPosts, getAllComments, deleteComment } from '../../actions';
 import shortid from 'shortid';
 
 class PostComments extends Component {
@@ -12,6 +12,7 @@ class PostComments extends Component {
   render() {
     console.log("PostComments: id of post = ", this.props.root_id);
     console.log("Comments in Post Detail: ", this.props.comments);
+    console.log("History info: ", this.props)
     // let currentPost = this.props.posts[Object.keys(this.props.posts).filter(postKey => (
     //   (this.props.posts[postKey].id) === this.props.root_id
     // ))]
@@ -28,6 +29,9 @@ class PostComments extends Component {
             let comment=this.props.comments[comKey];
             let resp = (comment.parentId === this.props.root_id) ?
                 (<div className="row" key={shortid.generate()}>
+                  <button type="submit" onClick={() => { this.props.delete_comment(comment.id); }} className="btn-floating btn-tiny waves-effect waves-light red">
+                    <i className="material-icons tiny">close</i>
+                  </button>
                   <div className="col s8 offset-s2">
                     <div className="card-panel white">
                       <span className="black-text">
@@ -54,7 +58,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     get_all_posts: () => dispatch(getAllPosts()),
-    get_all_comments: (id) => dispatch(getAllComments(id))
+    get_all_comments: (id) => dispatch(getAllComments(id)),
+    delete_comment: (id) => dispatch(deleteComment(id))
   }
 }
 
